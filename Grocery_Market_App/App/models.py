411e8e669397ee17276f2c533042075538f8568a
Muskan Valmikee <muskan20@navgurukul.org>
2022-04-07 20:46:40 +0530

    first commit.

---
+++ Grocery_Market_App/App/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField("Name", max_length=240)
+    location = models.CharField("Location",max_length=240)
+    shopTiming = models.CharField("Shop Timing", max_length=240)
+
+    def __str__(self):
+        return self.id,self.name,self.location,self.shopTiming
