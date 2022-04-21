cb35b5123cc097fad4b81cbb478915c4846f1d43
MuskanValmiki <muskanvalmikee@gmail.com>
2022-04-21 14:06:58 +0530

    eight comment.

--- Grocery_Market_App/app/views.py
+++ Grocery_Market_App/app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import UniqueSerializer
+from .serializers import AppSerializer
 from .models import App
 from rest_framework.response import Response
 from django.shortcuts import render, get_object_or_404
-from serializers import AppSerializer
 
 # Create your views here.
 
